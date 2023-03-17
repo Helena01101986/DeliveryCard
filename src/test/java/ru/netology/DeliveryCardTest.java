697d6fe4961b2d0925c0705e8378e3a80ee08652
Helen Eremina <karpova01101986@gmail.com>
2023-03-17 12:18:59 +0300

    Test added

--- src/test/java/ru/netology/DeliveryCardTest.java
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -1,15 +1,41 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 public class DeliveryCardTest {
+
+
 
     @Test
     void shouldTestCardDeliveryOrder() {
 
+        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
+        $x("//input[@placeholder='Город']").setValue("Новосибирск");
+        $("[data-test-id=date] input").click();
+        //$x("//input[@placeholder='Дата встречи']").clear();
+        $("[data-test-id=date] input").setValue(Keys.CONTROL + "a" + Keys.DELETE);
+        $("[data-test-id=date] input").setValue("25.03.2023");
+        $x("//input[@name='name']").setValue("Иванов Петр");
+        $("[data-test-id=phone] input").setValue("+79311234567");
+        $("[data-test-id=agreement]").click();
+        $x("//span[contains(text(), 'Забронировать')]").click();
+        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(12));
+
+
+
+
+
+
+
+
+
     }
 
 }
